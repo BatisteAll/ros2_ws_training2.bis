13a1efe41216e0b824061e20807e3f66150db1c5
BatisteAll <allilaire.batiste@gmail.com>
2024-03-04 19:23:58 +0100

    hardware IF written

--- src/custom_armbot_hardware/include/custom_armbot_hardware/dof6bot_hardware.hpp
+++ src/custom_armbot_hardware/include/custom_armbot_hardware/dof6bot_hardware.hpp
@@ -18,16 +18,17 @@
 ////////////////////////
 //      INCLUDES      //
 ////////////////////////
-// include necessary dependencies
+// include tools
 #include "string"
 #include "unordered_map"
 #include "vector"
-
+// include hardware interface basic knowledge
 #include "hardware_interface/handle.hpp"
 #include "hardware_interface/hardware_info.hpp"
 #include "hardware_interface/system_interface.hpp"
 #include "hardware_interface/types/hardware_interface_return_values.hpp"
 #include "hardware_interface/types/hardware_interface_type_values.hpp"
+// include ROS2 and ROS2 lifecycle node
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp/macros.hpp"
 #include "rclcpp_lifecycle/node_interfaces/lifecycle_node_interface.hpp"
@@ -38,30 +39,30 @@ namespace custom_armbot_hardware
 {
 
     // Define the term "CallbackReturn" as the call to ROS2 Lifecycle NODE callback funtion
-    using CallbackReturn = rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn;
+    // using CallbackReturn = rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn;
 
     ////////////////////////
     //    CLASS SYSTEM    //
     ////////////////////////
     // --> the class extends one of the 3 interface types: system (multi dof), actuator (1 dof), sensor
     // e.g. here the robot is a multi dof system
-    class HARDWARE_INTERFACE_PUBLIC RobotSystem : public hardware_interface::SystemInterface
+    class HARDWARE_INTERFACE_PUBLIC dof6botSystem : public hardware_interface::SystemInterface
     {
         public:
 
         // Macro needed to use the custom hardware interface with shared pointers
-        RCLCPP_SHARED_PTR_DEFINITIONS(HARDWARE_INTERFACE_PUBLIC)
+        RCLCPP_SHARED_PTR_DEFINITIONS(dof6botSystem)
 
 
         // Functions defining the interaction with ROS to check that hardware params are properly set
 
         // Init function called after plugin is loaded
         // --> provides hardware URDF configuration using the `HardwareInfo` structure
-        CallbackReturn on_init(const hardware_interface::HardwareInfo & info) override;
+        hardware_interface::CallbackReturn on_init(const hardware_interface::HardwareInfo &) override;
 
         // Configure function, called after initialization
         // --> configure all state interfaces and "non-movement" command interfaces for them to be available to controllers
-        CallbackReturn on_configure(const rclcpp_lifecycle::State & previous_state) override;
+        hardware_interface::CallbackReturn on_configure(const rclcpp_lifecycle::State &) override;
 
         // Defines the state interfaces
         std::vector<hardware_interface::StateInterface> export_state_interfaces() override;
@@ -69,15 +70,23 @@ namespace custom_armbot_hardware
         // Defines the command interfaces
         std::vector<hardware_interface::CommandInterface> export_command_interfaces() override;
 
+        // activate function 
+        // --> performs final preparations to start executing e.g. acquiring access to hardware
+        hardware_interface::CallbackReturn on_activate(const rclcpp_lifecycle::State &) override;
+
+        // deactivate function
+        // --> cleanup and reverse the onActivate changes
+        hardware_interface::CallbackReturn on_deactivate(const rclcpp_lifecycle::State &) override;
+
         // Defines how to read data from hardware
-        return_type read(const rclcpp::Time & time, const rclcpp::Duration & period) override;
+        hardware_interface::return_type read(const rclcpp::Time & time, const rclcpp::Duration &) override;
 
         // Defines how to write controller command to hardware
-        return_type write(const rclcpp::Time & /*time*/, const rclcpp::Duration & /*period*/) override;
+        hardware_interface::return_type write(const rclcpp::Time & time, const rclcpp::Duration &) override;
 
 
-        protected:
-        /// The size of this vector is (standard_interfaces_.size() x nr_joints)
+        private:
+        // The size of this vector is (standard_interfaces_.size() x nr_joints)
         // These vectors store hardware controller commands and states information
         std::vector<double> joint_position_command_;
         std::vector<double> joint_velocities_command_;
--- src/custom_armbot_hardware/src/dof6bot_hardware.cpp
+++ src/custom_armbot_hardware/src/dof6bot_hardware.cpp
@@ -12,18 +12,19 @@
 ////////////////////////
 // include necessary dependencies
 #include "custom_armbot_hardware/dof6bot_hardware.hpp"
+#include "hardware_interface/types/hardware_interface_type_values.hpp"
 #include <string>
 #include <vector>
 
 //Namespace for understanding purposes
-namespace ros2_control_demo_example_7
+namespace custom_hardware_armbot
 {
 
 
-    // on_init function: initialization of all member variables and process parameters
+    // METHOD on_init: initialization of all member variables and process parameters
     // from "info" argument to see if every parameter is valid else ERROR
     //---------------------------------------------------------------------------------------
-    CallbackReturn RobotSystem::on_init(const hardware_interface::HardwareInfo & info)
+    hardware_interface::CallbackReturn dof6botSystem::on_init(const hardware_interface::HardwareInfo &)
     {
 
         if (hardware_interface::SystemInterface::on_init(info) != CallbackReturn::SUCCESS)
@@ -53,25 +54,82 @@ namespace ros2_control_demo_example_7
     //---------------------------------------------------------------------------------------
 
 
-
-
+    // METHOD on_configure: setup the communication to the hardware and set everything up
+    // so that the hardware can be activated;
     //---------------------------------------------------------------------------------------
-    std::vector<hardware_interface::StateInterface> RobotSystem::export_state_interfaces()
+    hardware_interface::CallbackReturn dof6botSystem::on_configure(const rclcpp_lifecycle::State &)
     {
+        // This is already done in the on_init
+        // for (uint i = 0; i < joint_position_.size(); i++)
+        // {
+        //     joint_position_[i] = 0;
+        //     joint_velocities_[i] = 0;;
+        //     joint_position_command_[i] = 0;
+        //     joint_velocities_command_[i] = 0;
+        //     ft_states_[i] = 0;
+        //     ft_command_[i] = 0;
+        // }
+
+        RCLCPP_INFO(rclcpp::get_logger("HardwareInterface"),"Successfully configured!")
+
+
+
+        return hardware_interface::CallbackReturn::SUCCESS;
+    }
+    //---------------------------------------------------------------------------------------
+
+
+
+    // METHOD on_activate: executed when the hardware is transitionned to the active state reade to be executed
+    //---------------------------------------------------------------------------------------
+    hardware_interface::CallbackReturn dof6botSystem::on_activate(const rclcpp_lifecycle::State &)
+    {
+        RCLCPP_INFO(rclcpp::get_logger("HardwareInterface"),"Successfully activated!")
+
+        return hardware_interface::CallbackReturn::SUCCESS;
+    }
+    //---------------------------------------------------------------------------------------
+
+
+    // METHOD on_deactivate: reverse on activate changes and cleanUp
+    //---------------------------------------------------------------------------------------
+    hardware_interface::CallbackReturn dof6botSystem::on_deactivate(const rclcpp_lifecycle::State &)
+    {
+        RCLCPP_INFO(rclcpp::get_logger("HardwareInterface"),"Successfully deactivated!")
+
+        return hardware_interface::CallbackReturn::SUCCESS;
+    }
+    //---------------------------------------------------------------------------------------
+
+
+    // METHOD export_state_interfaces: export a struct type vector containing joint names, IF types, current positions and velocities
+    //---------------------------------------------------------------------------------------
+    std::vector<hardware_interface::StateInterface> dof6botSystem::export_state_interfaces()
+    {
+        // defines the "state_interfaces" struct type variable which contains joint names, types of IF, actual state
         std::vector<hardware_interface::StateInterface> state_interfaces;
 
-        int ind = 0;
-        for (const auto & joint_name : joint_interfaces["position"])
+        // // fill "state_interfaces" with the joints positions
+        // int ind = 0;
+        // for (const auto & joint_name : joint_interfaces["position"])
+        // {
+        //     state_interfaces.emplace_back(joint_name, "position", &joint_position_[ind++]);
+        // }
+
+        // // fill "state_interfaces" with the joints velocities
+        // ind = 0;
+        // for (const auto & joint_name : joint_interfaces["velocity"])
+        // {
+        //     state_interfaces.emplace_back(joint_name, "velocity", &joint_velocities_[ind++]);
+        // }
+
+        for (uint i= 0; i < info_.joints.size(); i++)
         {
-            state_interfaces.emplace_back(joint_name, "position", &joint_position_[ind++]);
+            state_interfaces.emplace_back(hardware_interface::StateInterface(info_.joints[i].name, hardware_interface::HW_IF_POSITION, &joint_position_[i]));
+            state_interfaces.emplace_back(hardware_interface::StateInterface(info_.joints[i].name, hardware_interface::HW_IF_VELOCITY, &joint_velocities_[i]));
         }
 
-        ind = 0;
-        for (const auto & joint_name : joint_interfaces["velocity"])
-        {
-            state_interfaces.emplace_back(joint_name, "velocity", &joint_velocities_[ind++]);
-        }
-
+        // fill "state_interfaces" with the force torque sensor data
         state_interfaces.emplace_back("tcp_fts_sensor", "force.x", &ft_states_[0]);
         state_interfaces.emplace_back("tcp_fts_sensor", "force.y", &ft_states_[1]);
         state_interfaces.emplace_back("tcp_fts_sensor", "force.z", &ft_states_[2]);
@@ -84,23 +142,34 @@ namespace ros2_control_demo_example_7
     //---------------------------------------------------------------------------------------
 
     
+    // METHOD export_command_interfaces: export a struct type vector containing joint names, IF types, current positions and velocities
     //---------------------------------------------------------------------------------------
-    std::vector<hardware_interface::CommandInterface> RobotSystem::export_command_interfaces()
+    std::vector<hardware_interface::CommandInterface> dof6botSystem::export_command_interfaces()
     {
+        // defines the "command_interfaces" struct type variable which contains joint names, target states (positions, velocities)
         std::vector<hardware_interface::CommandInterface> command_interfaces;
 
-        int ind = 0;
-        for (const auto & joint_name : joint_interfaces["position"])
+        // // fill "command_interfaces" with the joints target positions
+        // int ind = 0;
+        // for (const auto & joint_name : joint_interfaces["position"])
+        // {
+        //     command_interfaces.emplace_back(joint_name, "position", &joint_position_command_[ind++]);
+        // }
+
+        // // fill "command_interfaces" with the joints target velocities
+        // ind = 0;
+        // for (const auto & joint_name : joint_interfaces["velocity"])
+        // {
+        //     command_interfaces.emplace_back(joint_name, "velocity", &joint_velocities_command_[ind++]);
+        // }
+
+        for (uint i= 0; i < info_.joints.size(); i++)
         {
-            command_interfaces.emplace_back(joint_name, "position", &joint_position_command_[ind++]);
+            state_interfaces.emplace_back(hardware_interface::CommandInterface(info_.joints[i].name, hardware_interface::HW_IF_POSITION, &joint_position_command_[i]));
+            state_interfaces.emplace_back(hardware_interface::CommandInterface(info_.joints[i].name, hardware_interface::HW_IF_VELOCITY, &joint_velocities_command_[i]));
         }
 
-        ind = 0;
-        for (const auto & joint_name : joint_interfaces["velocity"])
-        {
-            command_interfaces.emplace_back(joint_name, "velocity", &joint_velocities_command_[ind++]);
-        }
-
+        // fill "command_interfaces" with the target forces &  torques
         command_interfaces.emplace_back("tcp_fts_sensor", "force.x", &ft_command_[0]);
         command_interfaces.emplace_back("tcp_fts_sensor", "force.y", &ft_command_[1]);
         command_interfaces.emplace_back("tcp_fts_sensor", "force.z", &ft_command_[2]);
@@ -113,9 +182,9 @@ namespace ros2_control_demo_example_7
     //---------------------------------------------------------------------------------------
 
 
-
+    // METHOD read: 
     //---------------------------------------------------------------------------------------
-    return_type RobotSystem::read(const rclcpp::Time & /*time*/, const rclcpp::Duration & period)
+    hardware_interface::return_type dof6botSystem::read(const rclcpp::Time & time, const rclcpp::Duration &)
     {
         // TODO(pac48) set sensor_states_ values from subscriber
 
@@ -130,23 +199,30 @@ namespace ros2_control_demo_example_7
             joint_position_[i] = joint_position_command_[i];
         }
 
-        return return_type::OK;
+        return hardware_interface::return_type::OK;
     }
     //---------------------------------------------------------------------------------------
 
 
 
-
+    // METHOD write: 
     //---------------------------------------------------------------------------------------
-    return_type RobotSystem::write(const rclcpp::Time &, const rclcpp::Duration &)
+    hardware_interface::return_type dof6botSystem::write(const rclcpp::Time &, const rclcpp::Duration &)
     {
-        return return_type::OK;
+        return hardware_interface::return_type::OK;
     }
     //---------------------------------------------------------------------------------------
+
+
 
 }  // namespace custom_armbot_hardware
 
+
+
+////////////////////////
+//     PLUGINLIB      //
+////////////////////////
+// include "PLUGINLIB_EXPORT_CLASS" macro to tell pluginlib that we are exporting this custom hardware class as ROS2 plugin
 #include "pluginlib/class_list_macros.hpp"
 
-PLUGINLIB_EXPORT_CLASS(
-  ros2_control_demo_example_7::RobotSystem, hardware_interface::SystemInterface)
+PLUGINLIB_EXPORT_CLASS(custom_hardware_armbot::dof6botSystem, hardware_interface::SystemInterface)
